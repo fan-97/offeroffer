9531c70652ccf769be2a9555059a52f0be4719b5
jie.fan <fann0205@gmail.com>
2021-08-27 17:34:31 +0800

    feat:添加1.7的hashmap的代码

--- javase/src/main/java/com/ccollection/HashMapTest.java
+++ javase/src/main/java/com/ccollection/HashMapTest.java
@@ -9,32 +9,42 @@ import java.util.Map;
  */
 public class HashMapTest {
     public static void main(String[] args) {
-        HashMap <String,Integer>map = new HashMap<>();
-        map.put("zhangsan01",23);
-        map.put("zhangsan02",22);
-        map.put("zhangsan03",33);
+        System.out.println(mod1(10, 3));
+        System.out.println(mod2(10, 3));
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("zhangsan01", 23);
+        map.put("zhangsan02", 22);
+        map.put("zhangsan03", 33);
 
         //获取所有的key  keySet()
         System.out.println("=====================keySet()方式==================");
-        for(String s:map.keySet()){
-            System.out.println(s+"...."+map.get(s));
+        for (String s : map.keySet()) {
+            System.out.println(s + "...." + map.get(s));
         }
 
         //获取所有的value
         System.out.println("=====================values()方式==================");
-        for(Integer i:map.values()){
-            System.out.println("value  "+i);
+        for (Integer i : map.values()) {
+            System.out.println("value  " + i);
         }
 
         //foreach entrySet
         System.out.println("=====================foreach entrySet==================");
-        for(Map.Entry<String,Integer> entry:map.entrySet()){
-            System.out.println(entry.getKey()+"...."+entry.getValue());
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "...." + entry.getValue());
         }
         System.out.println("=====================1.8新特性 lambda==================");
 
-        map.forEach((key,value)->{
-            System.out.println(key+"...."+value);
+        map.forEach((key, value) -> {
+            System.out.println(key + "...." + value);
         });
+    }
+
+    public static int mod1(int n, int d) {
+        return n % d;
+    }
+
+    public static int mod2(int n, int d) {
+        return (n - 1) & d;
     }
 }
